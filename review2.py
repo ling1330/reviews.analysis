af17f5c70de89a96d70999e3e788e13b89274e5d
ling1330 <monkeytree176@gmail.com>
2022-04-25 11:12:38 +0800

    review add

---
+++ review2.py
@@ -0,0 +1,23 @@
+#讀檔案
+data = []
+count = 0
+with open ('original.txt', 'r')as f:
+	for line in f:
+		data.append(line)
+		count = count + 1
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取完了','總共有',len(data)), '筆資料'\
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len +len(d)
+print('平均留言長度',sum_len/len(data))
+
+#篩選清單<100
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'留言長度小於100')
+print(new[0])
